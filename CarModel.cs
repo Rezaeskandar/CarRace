fb3189378b887b137ab8f568385b41bad7848122
Reza Eskandari <rezaeskandari.ammori@yahoo.com>
2023-04-05 01:01:11 +0200

    Version 1.0 car race Async Await

---
+++ CarModel.cs
@@ -0,0 +1,124 @@
+﻿using System.Xml.Linq;
+
+namespace CarRace
+{
+    internal partial class Program
+    {
+        public class CarModel
+        {
+            public string name { get; set; }
+            public decimal distance { get; set; }
+            public int speed { get; set; } = 0;
+            public int targetDistance { get; set; } = 1000;
+            public decimal tick { get; set; } = 30M;
+            public Random random { get; set; }
+            public CarModel(string name)
+
+            {
+                this.name = name;
+                distance = 0;
+                random = new Random();
+                speed = 120;
+            }
+
+            public async Task StartRace()
+            {
+                Console.WriteLine($"{name} startar!");
+
+                while (distance < targetDistance)
+                {
+                    // Vänta 30 sekunder
+                    for (int i = 0; i < 10; i++)
+                    {
+
+                        if (i % 4 == 0)
+                        {
+                            Console.Write("\r");
+                        }
+                        else if (i % 4 == 1)
+                        {
+                            Console.Write(".");
+                        }
+                        else if (i % 4 == 2)
+                        {
+                            Console.Write("..");
+                        }
+                        else
+                        {
+                            Console.Write("...");
+                        }
+
+                         await Task.Delay(300);
+                    }  
+
+                    // slumpa händelse
+                    int eventChance = random.Next(50);
+                    if (eventChance == 0)
+                    {
+                        await outOfGas();
+                    }
+                    else if (eventChance <= 2)
+                    {
+                        await flatTire();
+                    }
+                    else if (eventChance <= 7)
+                    {
+                        await dirtyWindshield();
+                    }
+                    else if (eventChance <= 17)
+                    {
+                        await engineProblem();
+                    }
+                    else
+                    {
+                      
+                    }
+
+                    // Uppdatera avstånd
+
+                    //distance += speed;
+
+                    //// Skriv ut status
+                    //Console.WriteLine($"{name} har nu kört {distance} i {tick} meter och kör i {speed} km/h. ");
+                    // calculate distance based on current speed and elapsed time
+                    decimal distanceMoved = (speed / 3.6M); // 3.6 is the conversion factor from km/h to m/s
+                    distance += distanceMoved;
+                    Console.WriteLine($"{name} har nu kört {distance:N0} m/s  meter och kör i {speed} km/h. ");
+                    //Console.Write("\r{0,-15} [{1}] {2:N2} m/s, {3:N2} km/h, {4:N2} m",
+                    //    name, new string('.', (int)(distance / targetDistance * 10)), speed / 3.6, speed, distance);
+                }
+
+            }
+            private async Task outOfGas()
+            {
+                Console.WriteLine($"\n{name} har slut på bensin och behöver tanka!");
+                await Task.Delay(300);
+                Console.WriteLine($"{name} har nu tankat och fortsätter köra.");
+            }
+
+            private async Task flatTire()
+            {
+                Console.WriteLine($"\n{name} har fått punktering och behöver byta däck!");
+                await Task.Delay(200);
+                Console.WriteLine($"{name} har nu bytt däck och fortsätter köra.");
+            }
+            private async Task dirtyWindshield()
+            {
+                Console.WriteLine($"\n{name} har fått en fågel på vindrutan och behöver tvätta den!");
+                await Task.Delay(100);
+                Console.WriteLine($"{name} har nu tvättat vind");
+            }
+            private async Task engineProblem()
+            {
+                Console.WriteLine($"\n{name} har fått motorproblem och hastigheten sänks med 1 km/h.");
+                speed -= 1;
+                await Task.Delay(300);
+                Console.WriteLine($"{name} har nu åtgärdat problemet och fortsätter köra i {speed} km/h.\n");
+            }
+
+           
+        }
+    
+    
+    }
+ }
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Linq;
+
+namespace CarRace
+{
+    internal partial class Program
+    {
+        static async Task Main(string[] args)
+        {
+            List<CarModel> Cars = new List<CarModel>();
+            CarModel car1 = new CarModel("Car 1");
+            CarModel car2 = new CarModel("Car 2");
+            CarModel car3 = new CarModel("Car 3");
+            CarModel car4 = new CarModel("Car 4");
+            CarModel car5 = new CarModel("Car 5"); 
+ 
+
+            Task task1 = car1.StartRace();
+            Task task2 = car2.StartRace();
+            Task task3 = car3.StartRace();
+            Task task4 = car4.StartRace();
+            Task task5 = car5.StartRace();
+
+            Cars.Add(car1);
+            Cars.Add(car2);
+            Cars.Add(car3);
+            Cars.Add(car4);
+            Cars.Add(car5);
+
+            await Task.WhenAll(task1, task2, task3, task4, task5);
+
+            Console.WriteLine("\nThe race has ended!");
+
+            //Hitta vinnaren
+                int maxDistance = -1;
+            CarModel? winner = null;
+            foreach (CarModel car in Cars)
+            {
+                if (car.distance > maxDistance)
+                {
+                    maxDistance = (int)car.distance;
+                    winner = car;
+                }
+            }
+            Console.ForegroundColor= ConsoleColor.Green;
+            Console.WriteLine($"\n{winner.name} Won The Race!");
+            Console.ResetColor();
+
+
+
+        }
+    
+    
+    }
+ }
